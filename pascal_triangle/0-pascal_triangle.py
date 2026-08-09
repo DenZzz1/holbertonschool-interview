06121eab01c01c57bc04ba0a1d9e0b1ba2993ef8
DenZzz1 <m2nky2fficiel@gmail.com>
2026-08-09 17:53:23 +0000

    task 0

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Module pour générer le triangle de Pascal
+"""
+
+
+def pascal_triangle(n):
+    """
+    Retourne une liste de listes d'entiers représentant
+    le triangle de Pascal de n.
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        prev_row = triangle[-1]
+        current_row = [1]
+
+        for j in range(1, i):
+            # Somme des deux éléments au-dessus
+            val = prev_row[j - 1] + prev_row[j]
+            current_row.append(val)
+
+        current_row.append(1)
+        triangle.append(current_row)
+
+    return triangle
